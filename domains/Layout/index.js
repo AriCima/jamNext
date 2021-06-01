ff7316c61a4102fbfcb7b297f7a81e1820b2a6a4
Veenders <carles.vd@gmail.com>
2021-06-01 20:34:36 +0200

    Añadir Configuraciones Eslint

--- domains/Layout/index.js
+++ domains/Layout/index.js
@@ -1,19 +1,18 @@
-import React, { useState }from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
 import Link from 'next/Link';
 import firebase from 'firebase';
 import { useDispatch } from 'react-redux';
 
-import JamsList from "../JamsList";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faCheck, faUser } from '@fortawesome/free-solid-svg-icons';
+import JamsList from '../JamsList';
 import Modal from '../../components/Modal';
 import CreateForm from '../CreateForm';
 import JoinForm from '../JoinForm';
 import { resetUserInfo } from '../../redux/actions/userActions';
-import { Div, SubTitle, AppWrapper } from "../../styledComps";
-
+import { Div, SubTitle, AppWrapper } from '../../styledComps';
 
 const NavBarIcon = styled.p`
   font-size: 30px;
@@ -26,65 +25,63 @@ const NavBarIcon = styled.p`
 `;
 
 const Layout = ({ children, leftMenu }) => {
-
   const [showProfile, setShowProfile] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [showCreate, setShowCreate] = useState(true);
-  
+
   const router = useRouter();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const signOut = (e) => {
     e.preventDefault();
     firebase.auth().signOut()
-    .then(() => {
+      .then(() => {
       // alert('See you later');
       // localStorage.setItem('userId', '');
-      console.log('Logout')
-      dispatch(resetUserInfo());
-      router.push('/');
-    })
-    .catch(() => {
-      alert("Ups! Seems you'll have to stay longer")// An error happened.
-    });
+        console.log('Logout');
+        dispatch(resetUserInfo());
+        router.push('/');
+      })
+      .catch(() => {
+        alert("Ups! Seems you'll have to stay longer");// An error happened.
+      });
   };
 
   const showProfileMenu = () => {
     setShowProfile(!showProfile);
   };
 
-
   return (
     <AppWrapper>
-      <Div className="LeftSide" col flexG='1' maxW='30%'>
+      <Div className="LeftSide" col flexG="1" maxW="30%">
         {leftMenu}
       </Div>
 
-      <Div className="AppBody" col back="lightblue" flexG='3'>
-          {children}
+      <Div className="AppBody" col back="lightblue" flexG="3">
+        {children}
       </Div>
 
-      <Modal showModal={showModal} closeModal={()=>setShowModal(false)}>
-        <Div col w='100%' just="center" align="center" >
-            <Div col w="90%" pad="0 0 10px 0">
-                {showCreate ? (
-                    <>
-                        <Div className="LoginWrapper" w="100%" just="center">
-                            <SubTitle mgT="-5px" mgB="20px">Create yout own Jam !</SubTitle>
-                        </Div>
-                        <CreateForm showModal={val => setShowModal(val)}/>
-                    </>
-                ):(
-                    <>
-                        <Div className="RegisterWrapper" w="100%" just="center">
-                            <SubTitle mgT="-5px" mgB="20px">Input the jam-code</SubTitle>
-                        </Div>
-                        <JoinForm userId={'1'}/>
-                    </>
-                )}
-            </Div>
+      <Modal showModal={showModal} closeModal={() => setShowModal(false)}>
+        <Div col w="100%" just="center" align="center">
+          <Div col w="90%" pad="0 0 10px 0">
+            {showCreate ? (
+              <>
+                <Div className="LoginWrapper" w="100%" just="center">
+                  <SubTitle mgT="-5px" mgB="20px">Create yout own Jam !</SubTitle>
+                </Div>
+                <CreateForm showModal={(val) => setShowModal(val)} />
+              </>
+            ) : (
+              <>
+                <Div className="RegisterWrapper" w="100%" just="center">
+                  <SubTitle mgT="-5px" mgB="20px">Input the jam-code</SubTitle>
+                </Div>
+                <JoinForm userId="1" />
+              </>
+            )}
+          </Div>
         </Div>
-    </Modal>
+      </Modal>
 
     </AppWrapper>
   );
